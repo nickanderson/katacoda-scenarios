9d8adbe146e06c00a10026468f93948de367600d
Nick Anderson <nick@cmdln.org>
2017-08-18 22:40:08 -0500

    Update patch-allow-query-127.0.0.1.sh

--- cfengine-hub-install/patch-allow-query-127.0.0.1.sh
+++ cfengine-hub-install/patch-allow-query-127.0.0.1.sh
@@ -1,10 +1,12 @@
 #!/bin/env bash
 
-NEW_VER="      admit => { \"127.0.0.1\", @(def.policy_servers) };"
-CURRENT_VER="      admit => { @(def.policy_servers) };"
-AFTER="default_data_select_policy_hub"
-FILE1=/var/cfengine/share/NovaBase/masterfiles/controls/reports.cf
-FILE2=$FILE1
+export NEW_VER="      admit => { \"127.0.0.1\", @(def.policy_servers) };"
+export CURRENT_VER="      admit => { @(def.policy_servers) };"
+export AFTER="default_data_select_policy_hub"
+export FILE1=/var/cfengine/share/NovaBase/masterfiles/controls/reports.cf
+export FILE2=/tmp/reports.cf
+
 
 # https://stackoverflow.com/a/32391110/2854727
 sudo sed -e '/default_data_select_policy_hub/!b' -e ':a' -e "s/$CURRENT_VER/$NEW_VER/;t trail" -e 'n;ba' -e ':trail' -e 'n;btrail' $FILE1 | sudo tee $FILE2
+sudo mv $FILE2 $FILE1
