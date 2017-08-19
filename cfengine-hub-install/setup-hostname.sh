e0f499eef8cf450da51e87888ab10d8fd45aad7a
Nick Anderson <nick@cmdln.org>
2017-08-19 12:37:47 -0500

    Create setup-hostname.sh

---
+++ cfengine-hub-install/setup-hostname.sh
@@ -0,0 +1,2 @@
+sudo bash -c "echo 127.0.0.1 [[HOST_SUBDOMAIN]]-443-[[KATACODA_HOST]].environments.katacoda.com >> /etc/hosts"
+sudo hostnamectl set-hostname [[HOST_SUBDOMAIN]]-443-[[KATACODA_HOST]].environments.katacoda.com
